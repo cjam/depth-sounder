93a2380e352a74dfc7808332c19bd6b22a50c53d
Colter McQuay <colter.mcquay@gmail.com>
2016-03-20 15:46:29 -0700

    Have some rudimentary d3 interface working.  Am creating circles for each of the channels and binding the properties of the channelViewModel to the circles properties.  Then I'm using hammerjs to add events to the instances of svg shapes that d3 adds.. wow. pretty excited about this.

--- web/static/knockout-extensions.js
+++ web/static/knockout-extensions.js
@@ -55,3 +55,24 @@ ko.bindingHandlers.bootstrapSwitch = {
     }
 };
 
+ko.dirtyFlag = function(root, isInitiallyDirty) {
+    var result = function() {},
+        _initialState = ko.observable(ko.toJSON(root)),
+        _isInitiallyDirty = ko.observable(isInitiallyDirty);
+
+    result.isDirty = ko.computed(function() {
+        return _isInitiallyDirty() || _initialState() !== ko.toJSON(root);
+    });
+
+    result.reset = function(data) {
+        if(!data){
+            data = ko.toJSON(root);
+        }
+        _initialState(data);
+        _isInitiallyDirty(false);
+    };
+
+
+
+    return result;
+};
